e521341438f3778743f0351088f3c23dc9366071
spex2024 <dercolbags@gmail.com>
2024-09-26 12:13:49 +0000

    v1.0.0.0

--- route/auth/orders.js
+++ route/auth/orders.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import {cancelOrder, completeOrder, placeOrder} from "../../controller/order.js";
 import authenticate from "../../middleware/protected.js";
 import {getAllOrders} from "../../controller/admin.js";
+import {authAdmin} from "../../middleware/admin.js";
 
 // Adjust the path as necessary
 
@@ -11,7 +12,7 @@ const router = Router();
 router.post('/order', authenticate, placeOrder);
 router.post('/complete',authenticate, completeOrder);
 router.post('/cancel',authenticate, cancelOrder);
-router.get('/orders',authenticate, getAllOrders);
+router.get('/orders',authAdmin, getAllOrders);
 // router.post('/register', signUp);
 // router.get('/verify/:token', verifyEmail);
 // router.post('/resend', resendVerificationEmail);
