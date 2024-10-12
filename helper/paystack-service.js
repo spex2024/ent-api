0e46149c0181c4f8fda6ffc8cec5501e79216c97
spex2024 <dercolbags@gmail.com>
2024-10-12 20:30:05 +0000

    v1.0.0.0

--- helper/paystack-service.js
+++ helper/paystack-service.js
@@ -1,10 +1,10 @@
-// paystackService.js
 import Paystack from 'paystack-node';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const APIKEY = process.env.PAYSTACK_SECRET; // Load from environment variables
-const paystack = new Paystack(APIKEY);
+const APIKEY = process.env.PAYSTACK_SECRET; // Ensure this is correct
+const environment = process.env.PAYSTACK_ENV || 'test'; // default to 'test'
 
+const paystack = new Paystack(APIKEY, environment); // Initialize with environment
 export default paystack;
