d1c3f506d98f077cfc53f53b5ff58eb2d4f0552c
spex2024 <dercolbags@gmail.com>
2024-10-25 18:55:10 +0000

    v1.0.0.0

--- controller/check-payment-plan.js
+++ controller/check-payment-plan.js
@@ -1,10 +1,11 @@
 import checkAgencySubscriptions from "../helper/check-installment.js";
+import {checkInstallment} from "../helper/update-status.js";
 
 
 export const checkPaymentPlan = async (req , res) => {
 
     try{
-    await checkAgencySubscriptions()
+    await checkInstallment()
      return res.status(200).json({message:"Checking Installment Payment "})
 
     }
